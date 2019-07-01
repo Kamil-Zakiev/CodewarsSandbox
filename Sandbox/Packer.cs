b851b5ceea547ecf53e96cb384d25c366dda779a
Kamil Zakiev <kamil.m.zakiev@akvelon.com>
2019-07-01 17:46:12 +0300

    added packer task placeholder

---
+++ Sandbox/Packer.cs
@@ -0,0 +1,29 @@
+using System.Linq;
+using Sandbox.HelperUtils;
+using Xunit;
+
+namespace Sandbox
+{
+    /// <summary>
+    /// https://www.codewars.com/kata/5a51717fa7ca4d133f001fdf
+    /// </summary>
+    [Tag(Category.Algorithms | Category.DynamicProgramming)]
+    public class Packer
+    {
+        public static int PackBagpack(int[] scores, int[] weights, int capacity)
+        {
+            var items = scores.Zip(weights, (score, weight) => (score, weight)).ToArray();
+            
+            
+            
+            return 0;
+        }
+        
+        [Fact]
+        public static void ExampleTests()
+        {
+            Assert.Equal(29, PackBagpack(new[] {15, 10, 9, 5}, new[] {1, 5, 3, 4}, 8));
+            Assert.Equal(60, PackBagpack(new[] {20, 5, 10, 40, 15, 25}, new[] {1, 2, 3, 8, 7, 4}, 10));
+        }
+    }
+}
