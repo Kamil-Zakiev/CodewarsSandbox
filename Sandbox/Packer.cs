772a8d60cbac9401e3d20ccc13bd9b4cf420a7b2
Kamil Zakiev <kamil.m.zakiev@akvelon.com>
2019-07-05 21:05:03 +0300

    started Packer

--- Sandbox/Packer.cs
+++ Sandbox/Packer.cs
@@ -14,16 +14,38 @@ namespace Sandbox
         {
             var items = scores.Zip(weights, (score, weight) => (score, weight)).ToArray();
             
+            return A(items, capacity);
+        }
+
+        public static int A((int price, int weight)[] items, int capacity)
+        {
+            if (capacity <= 0 || items.Length == 0)
+            {
+                return 0;
+            }
             
+            var (price, weight) = items.Last();
+            var exceptLast = items.SkipLast(1).ToArray();
+            var a = A(exceptLast, capacity);
+            var leftCapacity = capacity - weight;
+            if (leftCapacity < 0)
+            {
+                return a;
+            }
             
-            return 0;
+            var b = price + A(exceptLast, leftCapacity);
+
+            return a > b ? a : b;
         }
         
-        [Fact(Skip = "Not implemented yet.")]
+        [Fact]
         public static void ExampleTests()
         {
             Assert.Equal(29, PackBagpack(new[] {15, 10, 9, 5}, new[] {1, 5, 3, 4}, 8));
-            Assert.Equal(60, PackBagpack(new[] {20, 5, 10, 40, 15, 25}, new[] {1, 2, 3, 8, 7, 4}, 10));
+            Assert.Equal(60, PackBagpack(
+                new[] {20, 5, 10, 40, 15, 25}, 
+                new[] { 1, 2,  3,  8,  7,  4},
+                10));
         }
     }
 }
